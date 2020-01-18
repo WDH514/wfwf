401e73d083a394292e9a10c1c6cd6b5664ae23be
WDH514 <1691708898@qq.com>
2020-01-18 13:28:24 +0800

    ListView点击事件

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this,android.R.layout.simple_list_item_1,data);*/
         ListView listView=(ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit=fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void initFruits(){
         for (int i=0;i<8;i++){
