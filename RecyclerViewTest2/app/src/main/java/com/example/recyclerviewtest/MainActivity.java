c47b66f08daec262c1604a6d84c8a860a4cbf8a4
WDH514 <1691708898@qq.com>
2020-01-18 21:59:25 +0800

    RecyclerView基本用法

---
+++ RecyclerViewTest2/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.recyclerviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList=new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFruits();
+        RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter=new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+    }
+    private void initFruits(){
+        for (int i=0;i<8;i++) {
+            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            fruitList.add(banana);
+            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            fruitList.add(orange);
+        }
+    }
+}
