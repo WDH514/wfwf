ea9a0638deaf5c8ad6306be5001cece3b5cbdcf7
WDH514 <1691708898@qq.com>
2020-01-19 22:04:57 +0800

    动态添加碎片

--- FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//横屏
+        Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+                default:
+                    break;
+        }
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        transaction.commit();
     }
 }
