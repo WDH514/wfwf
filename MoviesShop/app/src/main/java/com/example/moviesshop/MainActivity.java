70530660cece18426e81d2445d9d15a8aa63a9a4
WDH514 <1691708898@qq.com>
2020-01-31 17:41:06 +0800

    123

--- MoviesShop/app/src/main/java/com/example/moviesshop/MainActivity.java
+++ MoviesShop/app/src/main/java/com/example/moviesshop/MainActivity.java
@@ -1,9 +1,8 @@
-package com.example.materialtest;
+package com.example.moviesshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
