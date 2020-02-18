add38a620f9306c329f2129ba575d60290c3ca3f
WDH514 <1691708898@qq.com>
2020-02-18 15:37:25 +0800

    RecyclerView

--- CiXiu/app/src/main/java/com/example/cixiu/MainActivity.java
+++ CiXiu/app/src/main/java/com/example/cixiu/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -43,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            //设置全屏和状态栏透明
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            // 设置状态栏为灰色
+            getWindow().setStatusBarColor(Color.parseColor("#696969"));
+            // getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_main);
         button1=(Button) findViewById(R.id.button1);
         button2=(Button) findViewById(R.id.button2);
